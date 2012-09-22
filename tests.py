0d31aff3b3ba89d70a4040f8718dfd114710fcd3
Karlisson Bezerra <macedobz@gmail.com>
2012-09-22 18:38:11 -0300

    Added more helper functions tests

--- tests.py
+++ tests.py
@@ -1,18 +1,17 @@
 # encoding: utf-8
 
-import ion
+import os
 import unittest
+import ion
 
-class IonTest(unittest.TestCase):
-    def setUp(self):
+class TestHelpers(unittest.TestCase):
+    def test_ion_parser(self):
         self.data_ion_file = '''title = Write your title here
 date = 12/09/2012
 content
 Write your content here'''
         with open('/tmp/data.ion', 'w') as f:
             f.write(self.data_ion_file)
-
-    def test_ion_parser(self):
         ion_data = ion.parse_ion_file('/tmp/data.ion')
         data_dict = {
             'title': 'Write your title here',
@@ -24,6 +23,34 @@ Write your content here'''
     def test_date_format(self):
         formated_date = ion.date_format('1347640769.0', '%d/%m/%Y')
         self.assertEqual('14/09/2012', formated_date)
+
+    def test_system_pathinfo(self):
+        system_path = os.path.join(os.getcwd(), ion.CFG['system_dir'])
+        config_path = os.path.join(system_path, ion.CFG['config_file'])
+        pathinfo = ion.system_pathinfo()
+        self.assertEqual((system_path, config_path), pathinfo)
+
+    def test_build_style(self):
+        permalink = 'http://localhost/'
+        filenames = 'main.css, print.css'
+        built_tags = ion.build_style_tags(filenames, permalink)
+        model = '<link rel="stylesheet" type="text/css" href="url" />\n'
+        result = ''
+        for tag in filenames.split(','):
+            url = permalink + tag.strip()
+            result += model.replace('url', url)
+        self.assertEqual(result, built_tags)
+    
+    def test_build_script(self):
+        permalink = 'http://localhost/'
+        filenames = 'main.js, jquery.js'
+        built_tags = ion.build_script_tags(filenames, permalink)
+        model = '<script src="url"></script>\n'
+        result = ''
+        for tag in filenames.split(','):
+            url = permalink + tag.strip()
+            result += model.replace('url', url)
+        self.assertEqual(result, built_tags)
 
     def tearDown(self):
         pass
