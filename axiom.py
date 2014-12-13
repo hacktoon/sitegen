3e2548ded808b4e3b99f0c4cfb1c23bc3db2eff6
Karlisson Bezerra <contact@hacktoon.com>
2014-12-13 15:23:12 -0200

    Extracted categorization classes

--- axiom.py
+++ axiom.py
@@ -21,7 +21,8 @@ from datetime import datetime
 import book_dweller
 
 from mem import MemReader
-from page import Page, PageList
+from paging import Page, PageList
+from categorization import Category, CategoryList
 from stamper.stamper import Stamper
 from alarum import (ValuesNotDefinedError, FileNotFoundError,
                         SiteAlreadyInstalledError, PageExistsError,
@@ -45,45 +46,6 @@ HTML_FILENAME = 'index.html'
 MODEL_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(MODEL_DIR, DATA_DIR)
 BASE_PATH = os.curdir
-
-
-class Category:
-    '''Define a category of pages'''
-    def __init__(self, name):
-        self.name = name
-        self.pages = PageList()
-
-    def add_page(self, page):
-        '''To add a book to a category'''
-        self.pages.insert(page)
-    
-    def paginate(self):
-        '''To sort books in the shelves'''
-        self.pages.paginate()
-
-
-class CategoryList:
-    '''Define a list of categories'''
-    def __init__(self):
-        self.items = {}
-
-    def __iter__(self):
-        for category in self.items.values():
-            yield category
-
-    def add_category(self, category_name):
-        '''To create a new category of books'''
-        if category_name in self.items.keys() or not category_name:
-            return
-        self.items[category_name] = Category(category_name)
-
-    def add_page(self, category_name, page):
-        '''To add a book to a specific category in a list'''
-        if not category_name:
-            return
-        if category_name not in self.items.keys():
-            self.add_category(category_name)
-        self.items[category_name].add_page(page)
 
 
 class Renderer:
---
+++ categorization.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+'''
+===============================================================================
+Mnemonix - The Static Publishing System of Nimus Ages
+
+Author: Karlisson M. Bezerra
+E-mail: contact@hacktoon.com
+URL: https://github.com/hacktoon/mnemonix
+License: WTFPL - http://sam.zoy.org/wtfpl/COPYING
+
+===============================================================================
+'''
+from paging import PageList
+
+class Category:
+    '''Define a category of pages'''
+    def __init__(self, name):
+        self.name = name
+        self.pages = PageList()
+
+    def add_page(self, page):
+        '''To add a book to a category'''
+        self.pages.insert(page)
+    
+    def paginate(self):
+        '''To sort books in the shelves'''
+        self.pages.paginate()
+
+
+class CategoryList:
+    '''Define a list of categories'''
+    def __init__(self):
+        self.items = {}
+
+    def __iter__(self):
+        for category in self.items.values():
+            yield category
+
+    def add_category(self, category_name):
+        '''To create a new category of books'''
+        if category_name in self.items.keys() or not category_name:
+            return
+        self.items[category_name] = Category(category_name)
+
+    def add_page(self, category_name, page):
+        '''To add a book to a specific category in a list'''
+        if not category_name:
+            return
+        if category_name not in self.items.keys():
+            self.add_category(category_name)
+        self.items[category_name].add_page(page)
