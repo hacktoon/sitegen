5ec9b025cd131e6779ba67faa4e0b63ded2a02f0
Karlisson Bezerra <cad_kbezerra@uolinc.com>
2015-04-20 17:57:15 -0300

    Initial tests for config reader tokenizer

--- tests.py
+++ tests.py
@@ -1,74 +1,43 @@
 # encoding: utf-8
 
+'''
+===============================================================================
+Infiniscribe - The Infinite Automaton Scriber of Nimus Ages
+
+Author: Karlisson M. Bezerra
+E-mail: contact@hacktoon.com
+URL: https://github.com/hacktoon/infiniscribe
+License: WTFPL - http://sam.zoy.org/wtfpl/COPYING
+===============================================================================
+'''
+
 import unittest
-import os
+from infiniscribe import reader
 
-import mnemonix
-import axiom
-import reader
+class TestReader(unittest.TestCase):
 
-from datetime import datetime
+    def setUp(self):
+        self.text = ('title = Post title\n'
+            'date = 2012-09-12 14:30:10\n'
+            'content\n'
+            'example text')
+        self.tokens = reader._tokenize(self.text)
 
-class TestFileHandlers(unittest.TestCase):
+    def testTokenizeSingleKeyValue(self):
+        text = ' title  =  Post title\n  '
+        tokens = reader._tokenize(text)
+        self.assertEqual(tokens[0]['type'], 'name')
+        self.assertEqual(tokens[0]['value'], 'title')
 
-	def setUp(self):
-		self.test_file_path = 'test_file'
-		self.test_file = open(self.test_file_path, 'w')
-		self.content = 'this is first line\nthis is second line'
-		self.test_file.write(self.content)
-		self.test_file.close()
+        self.assertEqual(tokens[1]['value'], '=')
+        self.assertEqual(tokens[0]['type'], 'name')
+        self.assertEqual(tokens[2]['value'], 'Post title')
 
-
-	def testReadFile(self):
-		content_read = axiom.read_file(self.test_file_path)
-		self.assertEqual(self.content, content_read)
-		
-
-	def testWriteFile(self):
-		content = 'Another version\nthis is another line'
-		axiom.write_file(self.test_file_path, content)
-		with open(self.test_file_path) as f:
-			test_content = f.read()
-			self.assertEqual(test_content, content)
-
-
-	def tearDown(self):
-		os.remove(self.test_file_path)
-
-
-class TestHelpers(unittest.TestCase):
-
-	def testFileParser(self):
-		file_content = ('title = Write your title here\n'
-	   'date = 2012-09-12 14:30:10\n'
-	   'content\n'
-	   'Write your content here')
-		file_data = axiom.parse_input_file(file_content)
-		data_dict = {
-			'title': 'Write your title here',
-			'date': '2012-09-12 14:30:10',
-			'content': 'Write your content here'
-		}
-		self.assertEqual(file_data, data_dict)
-
-
-	def testURLJoin(self):
-		# append slash
-		final = axiom.urljoin('http://localhost', '')
-		self.assertEqual('http://localhost/', final)
-		# concat url parts
-		final = axiom.urljoin('http://localhost', 'base')
-		self.assertEqual('http://localhost/base', final)
-		# split extra slashes
-		final = axiom.urljoin('http://localhost/', '/base/')
-		self.assertEqual('http://localhost/base', final)
-
-		
-	def testExtractTags(self):
-		tag_string = 'eletron,  neutron,  proton   '
-		tag_list = ['eletron', 'neutron', 'proton']
-		self.assertEqual(tag_list, axiom.extract_tags(tag_string))
+    def testTokenizerContentValue(self):
+        text = 'content\nExample text'
+        tokens = reader._tokenize(text)
+        self.assertEqual(tokens[0]['value'], 'Example text')
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
