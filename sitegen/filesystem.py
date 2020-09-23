ee9a55464e06d859872fe858735bca008208ce0b
Karlisson Bezerra <macedobz@gmail.com>
2020-09-23 00:10:27 -0300

    Define fileset interface

--- sitegen/filesystem.py
+++ sitegen/filesystem.py
@@ -3,23 +3,27 @@ import os
 from collections import defaultdict
 from functools import cached_property
 
-from pathlib import Path
+from pathlib import Path, PurePath
+
+
+DATA_FILE = 'page.me'
 
 
 class FileSet:
     def __init__(self, base_path):
         self.base_path = Path(base_path)
-        self.site_assets = File(self.base_path, 'assets')
-        self.site_build = File(self.base_path, 'build')
 
     def _file(self, name):
-        file = File(self.base_path, name)
+        file = File(self.base_path, '', name)
         return file if file.exists else NoFile()
 
-    def _file_tree(self, name):
-        base_path = self.base_path / name
-        for path_str, _, files in os.walk(base_path):
-            yield Path(path_str), files
+    def _filetree(self, name, suffix=''):
+        root_path = self.base_path / name
+        for path_str, _, _filenames in os.walk(root_path):
+            filenames = [name for name in _filenames if name.endswith(suffix)]
+            path = Path(path_str)
+            namespace = str(path.relative_to(root_path)).replace('.', '')
+            yield path, namespace, filenames
 
     @property
     def config(self):
@@ -27,55 +31,61 @@ class FileSet:
 
     @property
     def templates(self):
-        for path, files in self._file_tree('templates'):
-            for file in files:
-                yield File(path, file)
+        for path, namespace, filenames in self._filetree('templates', '.tpl'):
+            for filename in filenames:
+                yield File(path, namespace, filename)
 
     @property
     def assets(self):
-        for path, files in self._file_tree('assets'):
-            for file in files:
-                yield File(path, file)
+        for path, namespace, filenames in self._filetree('assets'):
+            for filename in filenames:
+                yield File(path, namespace, filename)
 
     @property
     def data(self):
-        for path, files in self._file_tree('data'):
-            file = File(path, 'page.me')
-            if file.exists:
-                yield file  # DataFile
+        for path, files in self._filetree('data'):
+            data_path = path / DATA_FILE
+            if data_path.exists():
+                css = path.glob('*.css')
+                js = path.glob('*.js')
+                yield list(css)
 
 
 class File:
-    def __init__(self, base_path, name):
-        self.base_path = base_path
-        self.path = base_path / name
+    def __init__(self, base_path, namespace='', filename=''):
+        self.path = base_path / namespace / filename
+        self.namespace = namespace
+        self.filename = filename
 
     @property
     def name(self):
-        return self.path.stem
+        return PurePath(self.filename).stem
 
     @property
     def type(self):
-        return self.path.suffix.strip('.')
+        return PurePath(self.filename).suffix.lstrip('.')
 
     @property
     def content(self):
         with open(self.path) as file:
             return file.read()
 
-    @property
-    def exists(self):
-        return self.path.exists()
-
     def __repr__(self):
         return str(self.path)
 
 
+class DataFile(File):
+    def __init__(self, base_path, data_file, files):
+        super().__init__(self, base_path, name)
+
+
 class NoFile(File):
-    @property
-    def content(self):
-        return f'File "{self.path}" not found'
+    def __init__(self):
+        super().__init__(self, PurePath())
 
     @property
-    def exists(self):
-        return False
+    def content(self):
+        return ''
+
+    def __repr__(self):
+        return self.__class__.__name__
