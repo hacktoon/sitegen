7da4033561d1b289800f0cbe324a6338bdf8c70c
Karlisson Bezerra <macedobz@gmail.com>
2024-12-26 15:31:46 -0300

    Refactor

--- sitegen/site.py
+++ sitegen/site.py
@@ -40,32 +40,29 @@ FORBIDDEN_DIRS = set((STATIC_DIR, TEMPLATES_DIR, FEED_DIR))
 
 class SiteGenerator:
     def __init__(self, props=None):
+        self.props = props or {}
         self.pagelist = PageList()
         self.category_list = CategoryList()
-        self.props = props or {}
         self.base_path = self.props.get('base_path', '')
         self.page_builder = PageBuilder(self.props)
 
-    def read_page(self, path):
-        '''Return the page data specified by path'''
-        data_file_path = self.props.get('data_file', DATA_FILE)
-        file_path = os.path.join(path, data_file_path)
-        if not os.path.exists(file_path):
-            return
+    def read_page_tree(self, path, parent_page=None):
+        if path in self.props.get('blocked_dirs'):
+            return {}
 
-        try:
-            page_data = reader.parse(utils.read_file(file_path))
-        except PageValueError as err:
-            raise PageValueError('In file {!r}: {}'.format(file_path, err))
-        page_data['path'] = path
+        page_data = self._read_page(path)
+        page = None
+        if page_data:
+            page = self._build_page(page_data, parent_page)
+            # add page to ordered list of pages
+            if not page.is_draft():
+                self.pagelist.insert(page)
+        for sub_page_path in self._read_subpages_list(path):
+            self.read_page_tree(sub_page_path, page or parent_page)
 
-        image_file_name = self.props.get('image_file', IMAGE_FILE)
-        image_path = os.path.join(path, image_file_name)
-        if os.path.exists(image_path):
-            page_data['image'] = image_file_name
-        return page_data
+        return self.pagelist
 
-    def build_categories(self):
+    def _build_categories(self):
         category_entry = self.props.get('categories', {})
         render_list = []
         base_url = self.props['base_url']
@@ -83,7 +80,7 @@ class SiteGenerator:
             })
         return render_list
 
-    def build_page(self, page_data, parent_page):
+    def _build_page(self, page_data, parent_page):
         page = self.page_builder.build(page_data, parent_page)
 
         category_id = page_data.get('category')
@@ -104,24 +101,25 @@ class SiteGenerator:
                 page.template = self.props.get('default_template')
         return page
 
-    def read_page_tree(self, path, parent_page=None):
-        if path in self.props.get('blocked_dirs'):
-            return {}
+    def _read_page(self, path):
+        '''Return the page data specified by path'''
+        data_file_path = self.props.get('data_file', DATA_FILE)
+        file_path = os.path.join(path, data_file_path)
+        if not os.path.exists(file_path):
+            return
 
-        page_data = self.read_page(path)
-        page = None
-        children = {}
-        if page_data:
-            page = self.build_page(page_data, parent_page)
-            # add page to ordered list of pages
-            if not page.is_draft():
-                self.pagelist.insert(page)
-        for sub_page_path in self.read_subpages_list(path):
-            child_info = self.read_page_tree(sub_page_path, page or parent_page)
-            if child_info:
-                children.update(child_info)
+        try:
+            page_data = reader.parse(utils.read_file(file_path))
+        except PageValueError as err:
+            raise PageValueError('In file {!r}: {}'.format(file_path, err))
+        page_data['path'] = path
+        image_file_name = self.props.get('image_file', IMAGE_FILE)
+        image_path = os.path.join(path, image_file_name)
+        if os.path.exists(image_path):
+            page_data['image'] = image_file_name
+        return page_data
 
-    def read_subpages_list(self, path):
+    def _read_subpages_list(self, path):
         for filename in os.listdir(path or os.curdir):
             fullpath = os.path.join(path, filename)
             if not os.path.isdir(fullpath):
@@ -132,7 +130,7 @@ class SiteGenerator:
                 continue
             yield fullpath
 
-    def write_html(self, page, env):
+    def _write_html(self, page, env):
         if 'nohtml' in page.props:
             return
         templates_dir = self.props.get('templates_dir', TEMPLATES_DIR)
@@ -151,18 +149,18 @@ class SiteGenerator:
         html_path = os.path.join(page.path, html_filename)
         utils.write_file(html_path, output)
 
-    def publish_page(self, page, env):
+    def _publish_page(self, page, env):
         try:
-            self.write_html(page, env)
+            self._write_html(page, env)
         except FileNotFoundError as error:
             raise FileNotFoundError('{} at page {!r}'.format(error, page.path))
 
-    def publish_feeds(self):
+    def _publish_feeds(self):
         env = { 'site': self.props, 'template_cache': {}}
-        file_path = self.write_feed(env, self.pagelist, 'rss')
+        file_path = self._write_feed(env, self.pagelist, 'rss')
         print("Generated RSS {!r}.".format(file_path))
 
-    def write_feed(self, env, pagelist, name):
+    def _write_feed(self, env, pagelist, name):
         if not len(pagelist):
             return
         template = Template(FEED_FILE, os.path.join(DATA_DIR, FEED_FILE))
@@ -199,22 +197,21 @@ class Site:
         self.props['base_url'] = os.environ.get('URL', BASE_URL)
 
         generator = SiteGenerator(self.props)
-        category_list = generator.build_categories()
+        categories = generator._build_categories()
 
-        generator.read_page_tree(f'{path}/data')
+        pages = generator.read_page_tree(f'{path}/data')
         for cat in generator.category_list:
             cat.paginate()
-        pages = generator.pagelist
         env = {
             'pages': [p for p in pages if p.is_listable()],
             'site': self.props,
-            'categories': category_list,
+            'categories': categories,
             'template_cache': {}
         }
 
         for page in pages:
             env['page'] = page
-            generator.publish_page(page, env)
+            generator._publish_page(page, env)
             print('Generated HTML {!r}.'.format(page.path))
-        generator.publish_feeds()
+        generator._publish_feeds()
         return pages
